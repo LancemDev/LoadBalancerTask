f81058d7356d160117b71697f49ead88b30f3619
LancemDev <munyaolance1@gmail.com>
2025-06-21 12:41:33 +0300

    :sparkles: Task 1 :white_check_mark:

---
+++ server/sever.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_socketio import SocketIO
+import os
+
+SERVER_ID = os.environ.get("SERVER_ID")
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app)
+
+
+@app.route('/home', methods=['GET'])
+def home(): 
+    return jsonify(message=f"Hello from Server: {SERVER_ID}", status="successful"), 200
+
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    return "", 200
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
+
